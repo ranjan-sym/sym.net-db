c2f55edd5628bf785536ef65284fe45443232e76
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-08-16 01:18:18 +0545

    Basic Instance implementation

--- src/main/java/net/symplifier/db/ModelInstance.java
+++ src/main/java/net/symplifier/db/ModelInstance.java
@@ -1,16 +1,59 @@
 package net.symplifier.db;
 
-import net.symplifier.db.columns.Column;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by ranjan on 7/28/15.
  */
-public class ModelInstance {
+public class ModelInstance implements Model {
+  private final ModelStructure structure;
 
-  private final Map<Column, Field> values = new HashMap<>();
+  @Override
+  public ModelStructure getStructure() {
+    return structure;
+  }
+
+  @Override
+  public <T> T get(Column<?, T> column, int level) {
+    return (T)set.rows[level].get(column.getIndex());
+  }
+
+  public <T> void set(Column<?, T> column, int level, T value) {
+    ModelRow row = set.rows[level];
+
+    int colIndex = column.getIndex();
+    Object chk = row.get(colIndex);
+
+    // Let's first see if its actually a change
+    if (value == chk || (value != null && value.equals(chk))) {
+      return;
+    }
+
+    if (!row.isModified()) {
+      row = set.rows[level] = row.copy();
+    }
+
+    row.set(colIndex, value, true);
+  }
+
+  public boolean isModified() {
+    for(ModelRow r:set.rows) {
+      if(r.isModified()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public class ModelSet {
+    private ModelRow[] rows;
+  }
+
+  private ModelSet set;
+
+  public ModelInstance(ModelStructure structure) {
+    this.structure = structure;
+  }
+
+
 
 
 
