935b81789b7dd1209da104ec9d22f985d102a6ac
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-08-15 22:41:56 +0545

    Model interface finalized

--- src/main/java/net/symplifier/db/Model.java
+++ src/main/java/net/symplifier/db/Model.java
@@ -1,28 +1,123 @@
 package net.symplifier.db;
 
-import net.symplifier.db.columns.Column;
-import net.symplifier.db.columns.ReferenceColumn;
+
 import net.symplifier.db.exceptions.ModelException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
+ * The interface that represents the model of the Database System.
+ *
  * Created by ranjan on 7/27/15.
  */
 public interface Model {
 
-  Schema getSchema();
+  /**
+   * Retrieve the structure of this model
+   *
+   * @return {@link ModelStructure} object containing the total information
+   *         about this model
+   */
+  ModelStructure getStructure();
 
-  <T> Field<T, ? , ? , ?> getField(Column<?, T> column);
+  /**
+   * Retrieve the value of the given column of this model
+   *
+   * @param column The column of which the value needs to be retrieved for this
+   *               model
+   * @param level The depth of the model in the hierarchy of the concrete
+   *              implementation. For an independent model, the level value is 0.
+   *              The value changes only in the case when we use multiple
+   *              parent -> child hierarchy in the database table (Generalization).
+   *              In which case the base table will have 0 level, the table that
+   *              extends this base will have level 1 and so forth. The value
+   *              will also be greater than 0 for interface table
+   * @param <T> The type of the value
+   * @return Value of the field
+   */
+  <T> T get(Column<?, T> column, int level);
 
-  <T extends Model> Reference<T> getReference(ReferenceColumn<?, T> column);
+  /**
+   * Default implementation for retrieving the value of the given column of this
+   * model. This method will not work in case of an interface model {@link ModelInterface}.
+   * The {@link ModelInterface#getImplementedValue(Column)} must be used in
+   * case of the ModelInterface columns.
+   *
+   * @param column The column of which the value needs to be retrieved
+   * @param <T> The type of the value
+   * @return Value of the field
+   */
+  default <T> T get(Column<?, T> column) {
+    return get(column, column.getLevel());
+  }
+
+  /**
+   * Set the value of the given column of this model.
+   *
+   * @param column The column of which the value needs to be set for this model
+   * @param level The depth of the model in the hierarchy of the concrete
+   *              implementations. See {@link ModelInterface#get(Column, int)}
+   *              for detail explanation.
+   * @param value The value that needs to be set
+   * @param <T> The type of the value
+   */
+  <T> void set(Column<?, T> column, int level, T value);
+
+  /**
+   * Default implementation for setting the value of the given column of this
+   * model. This method will not work in case of an interface model {@link ModelInterface}.
+   * The {@link ModelInterface#setImplementedValue(Column, Object)} must be
+   * used in case of the ModelInterface
+   *
+   * @param column The column of which the value needs to bet set for this model
+   * @param value The value that needs to be set
+   * @param <T> The type of the value
+   */
+  default <T> void set(Column<?, T> column, T value) {
+    set(column, column.getLevel(), value);
+  }
+
+
+  //TODO implement the following events through interceptor and not here
+  default void onValidateSelect() {
+
+  }
+
+  default void onValidateInsert() {
+
+  }
+
+  default void onValidateUpdate() {
+
+  }
+
+  default void onValidateSave() {
+
+  }
+
+  default void onValidateDelete() {
+
+  }
+
+  default void onInserted() {
+
+  }
+
+  default void onUpdated() {
+
+  }
+
+  default void onSaved() {
+
+  }
+
+  default void onDeleted() {
+
+  }
+//  <T> Field<T, ? , ? , ?> getField(Column<?, T> column);
+//
+//  <T extends Model> Reference<T> getReference(ReferenceColumn<?, T> column);
 
 
 //  public static final Logger LOGGER = LogManager.getLogger(Model.class);
@@ -118,7 +213,7 @@ public interface Model {
 //
 
   interface Factory<T extends Model> {
-    T create(Schema schema);
+    T create(ModelStructure<T> structure);
   }
 
   class DefaultFactory<T extends Model> implements Factory<T> {
@@ -128,18 +223,18 @@ public interface Model {
     }
 
     @Override
-    public T create(Schema schema) {
+    public T create(ModelStructure structure) {
       try {
-        Constructor<T> constructor = modelClass.getConstructor(Schema.class);
-        return constructor.newInstance(schema);
+        Constructor<T> constructor = modelClass.getConstructor(ModelStructure.class);
+        return constructor.newInstance(structure);
       } catch (NoSuchMethodException e) {
-        throw new ModelException(modelClass, "Constructor with Schema only not found", e);
+        throw new ModelException(modelClass, "Constructor with Structure not found", e);
       } catch (InvocationTargetException e) {
-        throw new ModelException(modelClass, "Error while running constructor(Schema)", e);
+        throw new ModelException(modelClass, "Error while running constructor(ModelStructure)", e);
       } catch (InstantiationException e) {
         throw new ModelException(modelClass, "Model cannot be instantiated", e);
       } catch (IllegalAccessException e) {
-        throw new ModelException(modelClass, "Constructor(schema) does not have proper access", e);
+        throw new ModelException(modelClass, "Constructor(ModelStructure) does not have proper access", e);
       }
     }
   }
