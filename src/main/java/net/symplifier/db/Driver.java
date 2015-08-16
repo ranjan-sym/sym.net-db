288cf2acd94e6c7259e2240eee4b56d4b01a890c
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-08-17 00:29:28 +0545

    Comment improved on Driver interface

--- src/main/java/net/symplifier/db/Driver.java
+++ src/main/java/net/symplifier/db/Driver.java
@@ -1,6 +1,9 @@
 package net.symplifier.db;
 
 /**
+ * The Driver interface that provides Database driver specific functionality
+ * to Schema
+ *
  * Created by ranjan on 7/27/15.
  */
 public interface Driver {
