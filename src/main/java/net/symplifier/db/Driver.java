0eaead4e9257cac55681607c9d481079a8eddb93
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-08-17 00:27:56 +0545

    Driver interface cleaned up

--- src/main/java/net/symplifier/db/Driver.java
+++ src/main/java/net/symplifier/db/Driver.java
@@ -1,19 +1,26 @@
 package net.symplifier.db;
 
-import net.symplifier.db.core.field.FieldGenerator;
-import net.symplifier.db.query.filter.FilterEntity;
-
 /**
  * Created by ranjan on 7/27/15.
  */
 public interface Driver {
 
-  Session createSession();
+  /**
+   * Provides the object that sets the value of the parameter in the query
+   * depending upon its type
+   *
+   * @param valueType The type of the object of which the setter is required
+   * @return A Driver specific parameter setter
+   */
+  Object getParameterSetter(Class valueType);
 
-  String getEntityText(FilterEntity entity);
-
-  <T, F extends Field<T, ?, ?, ?>> FieldGenerator<F> getFieldGenerator(Class<T> type);
-
-  <T, F extends Field<T,?, ?, ?>> FieldGenerator<F> getImmutableFieldGenerator(Class<T> type);
+  /**
+   * Provides the object that retrieves the value of a column from the query
+   * result depending upon its type
+   *
+   * @param valueType The type of the object of which the getter is required
+   * @return A Driver specific field value getter
+   */
+  Object getField(Class valueType);
 
 }
