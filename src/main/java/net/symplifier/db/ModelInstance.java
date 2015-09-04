519d08cbc352784f7afcad6f008af8b7f5b54b5d
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-09-04 10:51:33 +0545

    bugfix. forgot to include newly created relationaldata in the list

--- src/main/java/net/symplifier/db/ModelInstance.java
+++ src/main/java/net/symplifier/db/ModelInstance.java
@@ -424,6 +424,7 @@ public class ModelInstance<M extends ModelInstance> implements Model {
     RelationalData<V> d = (RelationalData<V>)hasManyData.get(relation);
     if (d == null) {
       d = new RelationalData<>();
+      hasManyData.put(relation, d);
     }
     d.newRecords.add(model);
     return model;
