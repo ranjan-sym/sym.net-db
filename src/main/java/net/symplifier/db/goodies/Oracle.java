988bd672c06e1230f60549d9fa810f84ee5597ee
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-09-27 19:17:52 +0545

    Added Helper class to retrieve meta data information easily

---
+++ src/main/java/net/symplifier/db/goodies/Oracle.java
@@ -0,0 +1,28 @@
+package net.symplifier.db.goodies;
+
+import net.symplifier.db.Schema;
+
+import java.util.HashMap;
+
+/**
+ * Created by ranjan on 9/27/15.
+ */
+public class Oracle {
+  private final static Oracle oracle = new Oracle();
+
+  public static Oracle get() {
+    return oracle;
+  }
+
+  private final HashMap<String, String> metaDataCache = new HashMap<>();
+
+  public synchronized String getMetaData(String name) {
+    if (metaDataCache.containsKey(name)) {
+      return metaDataCache.get(name);
+    } else {
+      String metaData = Schema.get().getModelStructure(name).getMetaData().toString();
+      metaDataCache.put(name, metaData);
+      return metaData;
+    }
+  }
+}
